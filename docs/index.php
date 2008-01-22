1a5e07f3dd4dddcaf4e36ccdc2b7af0f181bf308
Roger López <lopez.roger@gmail.com>
2008-01-22 02:13:18 +0000

    Updates to documentation.

---
+++ docs/index.php
@@ -0,0 +1,14 @@
+<?php
+// $Id$
+
+/** @mainpage Asset Developer Documentation
+ * Welcome to the Drupal Asset Module developer's documentation. This module
+ * aims to provide a single, common interface for managing and using all manner
+ * of digital assets within Drupal.  This is done by providing several hooks
+ * that can be implemented by other contrib modules to implement Methods for
+ * creating new assets, and Formatters for formatting existing assets.  
+ *
+ * - Asset Components
+ *   - @link asset Asset @endlink
+ * 
+ */
