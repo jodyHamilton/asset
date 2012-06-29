5acc1d937035af54f2507572fff7d0784c370657
Aleksey Khodakovskiy <aleksey.khodakovskiy@gmail.com>
2012-06-29 18:27:19 +0200

    Removed files from deprecated master branch.

--- tinymce/drupalasset/images/readme.txt
+++
@@ -1,3 +0,0 @@
-Icons from the Lullacons Icon Pack. 
-http://www.lullabot.com/articles/free_gpl_icons_lullacons_pack_1
-
--- tinymce/drupalasset/langs/en.js
+++
@@ -1,6 +0,0 @@
-// English lang variables
-
-tinyMCE.addToLang('drupalasset',{
-title : 'Drupal Asset',
-desc : 'Add an existing Drupal asset or upload a new one.'
-});
